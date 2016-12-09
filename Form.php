3d478c3b34f69fbcabfbd90c1d37cbc99b6c1894
pceuropa <info@pceuropa.net>
2016-12-09 18:29:47 +0000

    composer

---
+++ Form.php
@@ -0,0 +1,19 @@
+<?php
+namespace pceuropa\form;
+
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+
+class Form extends  {
+
+    public function init(){
+        
+	    parent::init();	
+    }
+	
+	
+    public static function Render(){
+
+	    return 'form';
+    }
+}
