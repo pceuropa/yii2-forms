f67c55ffa357f2b6762db0010daebec0d50e547e
Rafal Marguzewicz <info@pceuropa.net>
2017-06-29 11:48:35 +0100

    grid view

---
+++ views/module/gridview.php
@@ -0,0 +1,56 @@
+<?php
+use yii\helpers\Html;
+use yii\grid\GridView;
+$template =  ($buttonsEditOnIndex) ? '{update} {view} {delete} | {clone}':'{view}';
+ 
+
+?>
+
+<h1><?= Yii::t('builder', 'Forms') ?> <?= Html::a('<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>', ['create'], ['class' => 'btn btn-success btn-sm']) ?> </h1>
+
+<?= Gridview::widget([
+    'dataProvider' => $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => [
+        ['class' => 'yii\grid\SerialColumn'],
+        'title',
+        [
+            'attribute' => 'url',
+            'format' => 'html',
+            'value' => function ($model) {
+                        return  Html::a ( $model->url, ['view', 'url' => $model->url], ['target' => 'new']);
+                    },
+        ],[
+            'attribute' => 'answer',
+            'format' => 'html',
+            'value' => function ($model) {
+             $maximum = null;
+             if ($model->maximum !== null) { $maximum = ' /'. $model->maximum; } 
+             return html::a ('<span class="glyphicon glyphicon-th-list" aria-hidden="true"></span> '.$model->answer.$maximum, ['list', 'id' => $model->form_id]);
+             },
+        ],[
+            'attribute' => 'date_end',
+            'format' => 'datetime',
+        ],
+        ['class' => 'yii\grid\ActionColumn',
+            'buttons' => [
+		        'view' => function ($url, $model, $key) {
+					return Html::a ( '<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> ', ['view', 'url' => $model->url] );
+				},
+		        'clone' => function ($url, $model, $key) {
+					return Html::a ( Yii::t('builder', 'clone'), ['clone', 'id' => $model->form_id] );
+				},
+            ],
+			'template' => $template
+        ],
+     ],
+   ]);?>
+    
+   </div>
+	<div class="col-md-4 pull-right">
+    	<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> - <?= Yii::t('app', 'Edit form')  ?><br />
+    	<span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> - <?= Yii::t('app', 'View form')  ?><br />
+    	<span class="glyphicon glyphicon-trash" aria-hidden="true"></span> - <?= Yii::t('app', 'Delete form')  ?><br />
+    	<span class="glyphicon glyphicon-list" aria-hidden="true"></span> - <?= Yii::t('app', 'Data received from completed forms')  ?>
+	</div>
+</div>
